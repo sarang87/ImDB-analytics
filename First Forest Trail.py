79a04376464cdcc220d7543f70581b4a12628c10
DESKTOP-8PR4OIE\jo_sa <sarang87@vt.edu>
2016-12-08 18:00:42 -0500

    After adding data file description Separating the data cleaning and imputation code form the Random Forest Code

--- First Forest Trail.py
+++ First Forest Trail.py
@@ -8,47 +8,51 @@ Created on Wed Nov 23 14:28:29 2016
 import pandas as pd
 import numpy as numpy
 import csv as csv
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import Imputer
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.cross_validation import cross_val_score
 
 
-movie = pd.read_csv('all_num_IMDB_before_imputation.csv')
 
 
-
-#i = Imputer (strategy = 'median')
-#i.fit(movie)
-#imputed_sample = i.transform(movie)
-#
-#numpy.savetxt('afterImputation.csv', imputed_sample, delimiter = ",")
-
-
-train_df = pd.read_csv('train.csv')
-test_df = pd.read_csv('test.csv')
+train_df = pd.read_csv('train.csv', header = 0)
+test_df = pd.read_csv('test.csv', header = 0)
 actualGross_df = pd.read_csv('grossactuals.csv')
 
 train_data = train_df.values
 test_data = test_df.values
-print train_df
-#
-#print 'Training...'
-#forest = RandomForestClassifier(n_estimators=100)
-#
+
+
+print 'Training...'
+forest = RandomForestRegressor(n_estimators=200, max_features = 15, min_samples_leaf = 5, oob_score = True)
+
 #forest = forest.fit(train_data[0::,1::],train_data[0::,0])
+
+
 #ids = actualGross_df['id'].values
 #gross = actualGross_df['gross'].values
-#print len(gross)
-#print len(test_data)
-#
+
+
+
+#########CROSS VALIDATION
+scores = cross_val_score(forest,train_data[0::,1::], train_data[0::,0], cv = 10, scoring = 'mean_absolute_error')
+print scores.mean(),scores.std()*2
+
+
+
+#########
+#oob_prediction_for_train = forest.oob_score_
+#print(oob_prediction_for_train, train_data)
+
 #print 'Predicting...'
 #output = forest.predict(test_data).astype(float)
-#
-#
-#predictions_file = open("forestresults1.csv", "wb")
+
+#predictions_file = open("forestresults_200_10_8.csv", "wb")
 #open_file_object = csv.writer(predictions_file)
 #open_file_object.writerow(["Id","gross"])
 #open_file_object.writerows(zip(ids, output))
 #predictions_file.close()
 #print 'Done.'
 #
-#forest.score(test_data, gross)
+#accuracy = forest.score(test_data, gross)
+#print accuracy
